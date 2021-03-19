226a2e3f5a2dcd85bcfbf203dd8eb45f07c49b6a
out-fedoseev-va <vafedoseev@sberbank.ru>
2021-03-19 18:09:31 +0300

    remove unused

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { GraphContainer } from "./components/GraphContainer";
 import { DataProvider } from "./service/contexts/useData";
 import { Legend } from "./components/Legend";
 import { HintsProvider } from "./service/contexts/useHints";
-import { TimeInfo } from "./components/TimeInfo";
 import { IncidentInfo } from "./components/IncidentInfo";
 import { DropZone } from "./components/DropZone";
 
