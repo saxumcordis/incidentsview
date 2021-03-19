259309b1a2174405c7d2e745039d848a659ebb13
out-fedoseev-va <vafedoseev@sberbank.ru>
2021-03-19 16:05:45 +0300

    init IncidentInfo component

---
+++ src/components/IncidentInfo/IncidentInfo.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { useHints } from "../../service/contexts";
+
+import styles from "./IncidentInfo.module.scss";
+import { useData } from "../../service/contexts";
+import { COLORS } from "../../assets/COLORS";
+
+export const IncidentInfo = () => {
+  const { incidentHovered } = useHints();
+  const { data } = useData();
+  const edges = data!.edges;
+  const incident = data!.nodes.find((e) => e.node_id === incidentHovered);
+
+  const parent = edges.find((e) => e.node2 === incidentHovered);
+  const children = edges
+    .filter((e) => e.node1 === incidentHovered)
+    ?.map((e) => e?.node2);
+
+  return (
+    <div className={styles.incidentInfoContainer}>
+      {!!incident && (
+        <>
+          <span>{`Инцидент: ${incident.node_id}`}</span>
+          <span>{`Система: ${incident.system_name}`}</span>
+          <span>
+            Приоритет:{" "}
+            <span
+              style={{
+                background: COLORS[incident.priority_code[0]],
+                color: "#000",
+              }}
+            >
+              {incident.priority_code}
+            </span>
+          </span>
+          <br />
+          {parent && <span>Родитель: {parent.node1}</span>}
+          {!!children.length && <span>Дети: {children.join(",")}</span>}
+        </>
+      )}
+    </div>
+  );
+};
---
+++ src/components/IncidentInfo/index.ts
@@ -0,0 +1,1 @@
+export { IncidentInfo } from "./IncidentInfo";
