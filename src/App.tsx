242ec7dd3c515044b19211f59d133f2b5638a8fe
out-fedoseev-va <vafedoseev@sberbank.ru>
2021-03-19 18:28:41 +0300

    fix css

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,22 @@ import { HintsProvider } from "./service/contexts/useHints";
 import { IncidentInfo } from "./components/IncidentInfo";
 import { DropZone } from "./components/DropZone";
 
-import styles from './index.module.scss';
+import styles from "./index.module.scss";
 
 const App = () => {
   return (
     <div className={styles.container}>
       <DataProvider>
         <HintsProvider>
-          <div className={styles.column}> <Legend /><IncidentInfo /></div>
-          <div className={styles.column}><GraphContainer /></div>
-          <DropZone />
+          <div className={styles.column}>
+            {" "}
+            <Legend />
+            <IncidentInfo />
+            <DropZone />
+          </div>
+          <div className={styles.column}>
+            <GraphContainer />
+          </div>
         </HintsProvider>
       </DataProvider>
     </div>
